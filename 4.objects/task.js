985216b8e24a7105932d886336fc439db3459f12
Thor-ux <alongeayomidedaniel@gmail.com>
2025-02-06 02:00:07 -0500

    main

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,41 @@
-function Student(name, gender, age) {
-  
+class Student {
+    constructor(name, gender, age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.marks = [];
+    }
+    setSubject(subjectName) {
+        this.subject = subjectName;
+    }
+    addMarks(...marksToAdd) {
+        if (this.marks) {
+            this.marks.push(...marksToAdd);
+        }
+    }
+    getAverage() {
+        if (!this.marks || this.marks.length === 0) {
+            return 0;
+        }
+        const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+        return sum / this.marks.length;
+    }
+    exclude(reason) {
+        delete this.subject;
+        delete this.marks;
+        this.excluded = reason;
+    }
 }
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
+// {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Algebra"}
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
-
-Student.prototype.addMarks = function (...marks) {
-  
-}
-
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба');
+console.log(student2);
+// {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
---
+++ 5.classes/task.js
@@ -0,0 +1,200 @@
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this._state = 100;
+        this.type = null;
+    }
+
+    fix() {
+        this.state = Math.min(this.state * 1.5, 100);
+    }
+
+    set state(newState) {
+        if (newState < 0) {
+            this._state = 0;
+        } else if (newState > 100) {
+            this._state = 100;
+        } else {
+            this._state = newState;
+        }
+    }
+
+    get state() {
+        return this._state;
+    }
+}
+
+class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = "magazine";
+    }
+}
+
+class Book extends PrintEditionItem {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.author = author;
+        this.type = "book";
+    }
+}
+
+class NovelBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "novel";
+    }
+}
+
+class FantasticBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "fantastic";
+    }
+}
+
+class DetectiveBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "detective";
+    }
+}
+const sherlock = new PrintEditionItem(
+    "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 
+    2019, 
+    1008
+);
+
+console.log(sherlock.releaseDate); // 2019
+console.log(sherlock.state); // 100
+sherlock.fix();
+console.log(sherlock.state); // 100
+
+const picknick = new FantasticBook(
+    "Аркадий и Борис Стругацкие", 
+    "Пикник на обочине", 
+    1972, 
+    168
+);
+
+console.log(picknick.author); // "Аркадий и Борис Стругацкие"
+picknick.state = 10;
+console.log(picknick.state); // 10
+picknick.fix();
+console.log(picknick.state); // 15
+
+class Library {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book) {
+        if (book.state > 30) {
+            this.books.push(book);
+        }
+    }
+
+    findBookBy(type, value) {
+        return this.books.find(book => book[type] === value) || null;
+    }
+
+    giveBookByName(bookName) {
+        const index = this.books.findIndex(book => book.name === bookName);
+        if (index !== -1) {
+            return this.books.splice(index, 1)[0];
+        }
+        return null;
+    }
+}
+// Library
+const library = new Library("Central Library");
+
+// Add printed publications of different types
+library.addBook(new DetectiveBook("Arthur Conan Doyle", "Sherlock Holmes", 1887, 200));
+library.addBook(new FantasticBook("J.R.R. Tolkien", "The Lord of the Rings", 1954, 1178));
+library.addBook(new NovelBook("Jane Austen", "Pride and Prejudice", 1813, 432));
+library.addBook(new Magazine("National Geographic", 2023, 100));
+
+// Find a book published in 1919 or create one if necessary
+let book1919 = library.findBookBy("releaseDate", 1919);
+if (!book1919) {
+    book1919 = new NovelBook("John Dos Passos", "The Young Man's Adventure", 1919, 350);
+    library.addBook(book1919);
+}
+
+console.log("Books in library:", library.books.length);
+
+// Give any book
+const givenBook = library.giveBookByName("Pride and Prejudice");
+console.log("Given book:", givenBook.name);
+console.log("Books remaining in library:", library.books.length);
+
+// Damage issued book
+givenBook.state = 20;
+console.log("Book state after damage:", givenBook.state);
+
+// Restore issued book
+givenBook.fix();
+console.log("Book state after restoration:", givenBook.state);
+
+// Adding the restored book to the library
+library.addBook(givenBook);
+console.log("Books in library after returning:", library.books.length);
+
+class Student {
+    constructor(name) {
+        this.name = name;
+        this.marks = {};
+    }
+
+    addMark(mark, subject) {
+        if (mark < 2 || mark > 5) {
+            return;
+        }
+
+        if (!this.marks[subject]) {
+            this.marks[subject] = [];
+        }
+
+        this.marks[subject].push(mark);
+    }
+
+    getAverageBySubject(subject) {
+        if (!this.marks[subject] || this.marks[subject].length === 0) {
+            return 0;
+        }
+
+        const sum = this.marks[subject].reduce((acc, mark) => acc + mark, 0);
+        return sum / this.marks[subject].length;
+    }
+
+    getAverage() {
+        const subjects = Object.keys(this.marks);
+        if (subjects.length === 0) {
+            return 0;
+        }
+
+        const sum = subjects.reduce((acc, subject) => {
+            return acc + this.getAverageBySubject(subject);
+        }, 0);
+
+        return sum / subjects.length;
+    }
+}
+
+const student = new Student("Олег Никифоров");
+student.addMark(5, "химия");
+student.addMark(5, "химия");
+student.addMark(5, "физика");
+student.addMark(4, "физика");
+student.addMark(6, "физика");
+student.getAverageBySubject("физика");
+student.getAverageBySubject("биология");
+student.getAverage();
+
+console.log(student.getAverageBySubject("физика")); // 4.5
+console.log(student.getAverageBySubject("биология")); // 0
+console.log(student.getAverage()); // 4.75
