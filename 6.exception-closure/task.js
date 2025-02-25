bef6dfc311a74864f72522e38732a233429312d1
Thor-ux <alongeayomidedaniel@gmail.com>
2025-02-25 01:05:52 -0500

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿function parseCount(value) {
+    const parsedValue = Number.parseFloat(value);
+    if (isNaN(parsedValue)) {
+        throw new Error("Невалидное значение");
+    }
+    return parsedValue;
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+      if (a + b <= c || a + c <= b || b + c <= a) {
+        throw new Error("Треугольник с такими сторонами не существует");
+      }
+      this._a = a;
+      this._b = b;
+      this._c = c;
+    }
+  
+    get perimeter() {
+      return this._a + this._b + this._c;
+    }
+  
+    get area() {
+      const s = this.perimeter / 2;
+      return Number(Math.sqrt(s * (s - this._a) * (s - this._b) * (s - this._c)).toFixed(3));
+    }
+  }
+  
+  function getTriangle(a, b, c) {
+    try {
+      return new Triangle(a, b, c);
+    } catch (error) {
+      return {
+        get area() {
+          return "Ошибка! Треугольник не существует";
+        },
+        get perimeter() {
+          return "Ошибка! Треугольник не существует";
+        }
+      };
+    }
+  }
+  
