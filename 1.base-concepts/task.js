a17e32c4f7bb51cc33fa1bce0c8230bc2905d219
Thor-ux <alongeayomidedaniel@gmail.com>
2025-03-03 03:50:20 -0500

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,36 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
+"use strict";
+
+function solveEquation(a,  b,  c) {
+  const discriminant = b ** 2 - 4 * a * c;
   
-  return arr;
+  if (discriminant < 0) {
+    return [];
+  } else if (discriminant === 0) {
+    return [-b / (2 * a)];
+  } else {
+    const x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    const x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    return [x1,  x2];
+  }
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+function calculateTotalMortgage(percent,  contribution,  amount,  countMonths) {
+  // Преобразуем процентную ставку
+  const monthlyInterest = percent / 100 / 12;
   
+  // Рассчитываем тело кредита
+  const principal = amount - contribution;
+  
+  if (principal <= 0) {
+    return 0;
+  }
+  
+  // Рассчитываем ежемесячный платеж
+  const monthlyPayment = principal * (monthlyInterest + (monthlyInterest / (Math.pow(1 + monthlyInterest,  countMonths) - 1)));
+  
+  // Рассчитываем общую сумму
+  const totalAmount = monthlyPayment * countMonths;
+  
+  // Округляем до двух знаков после запятой
+  return Number(totalAmount.toFixed(2));
 }
