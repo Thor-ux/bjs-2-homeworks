20931d579372a6898677735222be450710ee6727
Thor-ux <alongeayomidedaniel@gmail.com>
2025-03-03 04:04:37 -0500

    Второе задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,55 +1,42 @@
 "use strict";
 
-function getArrayParams(...arr) {
-  if (arr.length === 0) {
-    return { min: 0, max: 0, avg: 0 };
-  }
-
-  let min = arr[0];
-  let max = arr[0];
-  let sum = 0;
-
-  for (let num of arr) {
-    if (num < min) min = num;
-    if (num > max) max = num;
-    sum += num;
-  }
-
-  const avg = Number((sum / arr.length).toFixed(2));
-
-  return { min, max, avg };
-}
-
-function summElementsWorker(...arr) {
-  if (arr.length === 0) return 0;
-  return arr.reduce((sum, num) => sum + num, 0);
-}
-
-function differenceMaxMinWorker(...arr) {
-  if (arr.length === 0) return 0;
-  return Math.max(...arr) - Math.min(...arr);
-}
-
-function differenceEvenOddWorker(...arr) {
-  if (arr.length === 0) return 0;
-  return arr.reduce((diff, num) => num % 2 === 0 ? diff + num : diff - num, 0);
-}
-
-function averageEvenElementsWorker(...arr) {
-  const evenNumbers = arr.filter(num => num % 2 === 0);
-  if (evenNumbers.length === 0) return 0;
-  return Math.round(evenNumbers.reduce((sum, num) => sum + num) / evenNumbers.length);
-}
-
-function makeWork(arrOfArr, func) {
-  let maxResult = -Infinity;
-
-  for (let arr of arrOfArr) {
-    const result = func(...arr);
-    if (result > maxResult) {
-      maxResult = result;
+const functions = {
+  getArrayParams: function(...arr) {
+    if (arr.length === 0) {
+      return { min: 0, max: 0, avg: 0 };
     }
-  }
+    const min = Math.min(...arr);
+    const max = Math.max(...arr);
+    const avg = Number((arr.reduce((sum, num) => sum + num, 0) / arr.length).toFixed(2));
+    return { min, max, avg };
+  },
 
-  return maxResult === -Infinity ? 0 : maxResult;
-}
+  summElementsWorker: function(...arr) {
+    return arr.reduce((sum, num) => sum + num, 0);
+  },
+
+  differenceMaxMinWorker: function(...arr) {
+    if (arr.length === 0) return 0;
+    return Math.max(...arr) - Math.min(...arr);
+  },
+
+  differenceEvenOddWorker: function(...arr) {
+    return arr.reduce((diff, num) => num % 2 === 0 ? diff + num : diff - num, 0);
+  },
+
+  averageEvenElementsWorker: function(...arr) {
+    const evenNumbers = arr.filter(num => num % 2 === 0);
+    if (evenNumbers.length === 0) return 0;
+    return Math.round(evenNumbers.reduce((sum, num) => sum + num) / evenNumbers.length);
+  },
+
+  makeWork: function(arrOfArr, func) {
+    return arrOfArr.reduce((max, arr) => {
+      const result = func(...arr);
+      return result > max ? result : max;
+    }, -Infinity);
+  }
+};
+
+// To make the functions globally accessible:
+Object.assign(window, functions);
